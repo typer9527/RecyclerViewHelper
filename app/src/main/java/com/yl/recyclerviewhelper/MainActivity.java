089eb80e3a088f986e2875c166d08af6d2edd50b
Yangli <yangl7240@gmail.com>
2017-11-07 10:49:13 +0800

    完成数据刷新和加载更多

--- app/src/main/java/com/yl/recyclerviewhelper/MainActivity.java
+++ app/src/main/java/com/yl/recyclerviewhelper/MainActivity.java
@@ -1,6 +1,8 @@
 package com.yl.recyclerviewhelper;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,18 +18,85 @@ public class MainActivity extends AppCompatActivity {
             new Fruit("Grape", R.drawable.grape), new Fruit("Orange", R.drawable.orange),
             new Fruit("Peach", R.drawable.peach), new Fruit("Tomato", R.drawable.tomato)};
     private List<Fruit> fruitList = new ArrayList<>();
+    private FruitsAdapter adapter;
+    private SwipeRefreshLayout refreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        refreshLayout = (SwipeRefreshLayout) findViewById(R.id.refresh_layout);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        refreshLayout.setColorSchemeColors(Color.BLUE, Color.RED);
         //recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        FruitsAdapter adapter = new FruitsAdapter(fruitList);
+        adapter = new FruitsAdapter(fruitList);
         recyclerView.setAdapter(adapter);
         refreshFruits();
         adapter.notifyDataSetChanged();
+        // 利用SwipeRefreshLayout实现刷新数据
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refresh();
+            }
+        });
+        // 利用OnScrollListener实现加载更多
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if (!(recyclerView.getScrollState() == RecyclerView.SCROLL_STATE_IDLE)
+                        && !recyclerView.canScrollVertically(1)) {
+                    loadMore();
+                }
+            }
+        });
+    }
+
+    private void loadMore() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            fruitList.addAll(getMoreFruits());
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void refresh() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            refreshFruits();
+                            adapter.notifyDataSetChanged();
+                            refreshLayout.setRefreshing(false);
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     private void refreshFruits() {
